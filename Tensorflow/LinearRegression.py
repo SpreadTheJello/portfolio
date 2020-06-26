2e17dc6a14b2a6f8a438eca8a4119b668a8645be
Angelo Fatali <spreadthejello@MacBook-Pro.lan>
2020-06-25 18:19:34 -0700

    added functions for each data areas

--- Tensorflow/LinearRegression.py
+++ Tensorflow/LinearRegression.py
@@ -11,7 +11,25 @@ import tensorflow as tf
 from tensorflow import feature_column
 
 # Load dataset
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
-dfeval=pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # train from
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # tests model
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
+
+def show_age():
+    dftrain.age.hist(bins=20)
+    plt.show()
+
+def show_sex():
+    dftrain.sex.value_counts().plot(kind='barh')
+    plt.show()
+
+def show_class():
+    dftrain['class'].value_counts().plot(kind='barh')
+    plt.show()
+
+def show_percentSurvivedBySex():
+    pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+    plt.show()
+
+show_percentSurvivedBySex()
