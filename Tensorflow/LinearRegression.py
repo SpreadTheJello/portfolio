9300fa63713f0295790fdfe2c9e47c1cd002e052
Angelo Fatali <spreadthejello@MacBook-Pro.lan>
2020-06-25 17:41:10 -0700

    loads dataset

--- Tensorflow/LinearRegression.py
+++ Tensorflow/LinearRegression.py
@@ -9,3 +9,9 @@ from six.moves import urllib
 import tensorflow as tf
 
 from tensorflow import feature_column
+
+# Load dataset
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfeval=pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
