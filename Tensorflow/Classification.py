503b8f00433f8ea4ad9511977758eac9f6bd2395
Angelo Fatali <spreadthejello@MacBook-Pro.lan>
2020-06-25 21:06:45 -0700

    added predictions

--- Tensorflow/Classification.py
+++ Tensorflow/Classification.py
@@ -50,4 +50,29 @@ classifier.train(
 eval_result = classifier.evaluate(
     input_fn=lambda: input_fn(test, test_y, training=False))
 
-print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+# print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+# makes predictions
+def input_fn(features, batch_size=256):
+    # convert the inputs to a dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print("Please type numeric values as prompted.")
+for feature in features:
+  valid = True
+  while valid: 
+    val = input(feature + ": ")
+    if not val.isdigit(): valid = False
+
+  predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn=lambda: input_fn(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        SPECIES[class_id], 100 * probability))
