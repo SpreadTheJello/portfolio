739f2a26c5ea6a8f3c42bd9f4bd27d2cfebac7a7
Angelo Fatali <spreadthejello@MacBook-Pro.lan>
2020-06-25 20:16:31 -0700

    finished

--- Tensorflow/LinearRegression.py
+++ Tensorflow/LinearRegression.py
@@ -16,6 +16,7 @@ dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.cs
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 
+# creates feature columns
 CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
                        'embark_town', 'alone']
 NUMERIC_COLUMNS = ['age', 'fare']
@@ -28,8 +29,7 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-print(feature_columns)
-
+# functions for individual graphs of the datasets
 def show_age():
     dftrain.age.hist(bins=20)
     plt.show()
@@ -46,3 +46,30 @@ def show_percentSurvivedBySex():
     pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
     plt.show()
 
+# input function: turns the panda dataset into the dataset object
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# creates the model
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+# trains the model
+linear_est.train(train_input_fn) # trains input function
+result = linear_est.evaluate(eval_input_fn) # gets model stats by evaluating on test data
+
+clear_output()
+print(result['accuracy'])
+print(result)
+
+result = list(linear_est.predict(eval_input_fn))
+print(dfeval.loc[3])
+print(result[3]['probabilities'][1])
