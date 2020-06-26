ac2ef8c1810ecb62a5118fa1079b81853aa6e657
Angelo Fatali <spreadthejello@MacBook-Pro.lan>
2020-06-25 17:32:14 -0700

    added imports

--- Tensorflow/LinearRegression.py
+++ Tensorflow/LinearRegression.py
@@ -1,8 +1,11 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from IPython.display import clear_output
+from six.moves import urllib
 
-x = [1, 2, 2.5, 3, 4]
-y = [1, 4, 7, 9, 15]
-plt.plot(x, y, 'ro')
-plt.axis([0, 6, 0, 20])
-plt.show()
+import tensorflow as tf
+
+from tensorflow import feature_column
