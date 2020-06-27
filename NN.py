7704dd1b44b8206feb606f90e8e9ec56ff8461ce
Angelo Fatali <spreadthejello@MacBook-Pro.lan>
2020-06-26 18:51:01 -0700

    listed data

--- NN.py
+++ NN.py
@@ -1,4 +1,22 @@
-import numpy
+# Problem: have data of 9 flowers, with a length, width, and a color (red or blue)
+# However, one of those flowers has a missing color.
+# This NN will get to the closest probability for the color of the mystery flower.
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+# each point is length, width, type (0 blue, 1 red) for a flower
+data = [[3,   1.5, 1],
+        [2,   1,   0],
+        [4,   1.5, 1],
+        [3,   1,   0],
+        [3.5,  .5, 1],
+        [2,    .5, 0],
+        [5.5, 1,   1],
+        [1,   1,   0]]
+
+# this is the flower with the missing color
+mystery_flower = [4.5, 1]
 
 def NN(m1, m2, w1, w2, b):
     z = m1 * w1 + m2 * w2 + b
